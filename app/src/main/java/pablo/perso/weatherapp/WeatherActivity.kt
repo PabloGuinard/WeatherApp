5d07c34251ec30976507b00b25744b52eaa376ab
Pablo Guinard <guinardpablo@gmail.com>
2022-11-26 15:23:31 +0100

    add time& move city to top right corner

--- app/src/main/java/pablo/perso/weatherapp/WeatherActivity.kt
+++ app/src/main/java/pablo/perso/weatherapp/WeatherActivity.kt
@@ -3,16 +3,22 @@ package pablo.perso.weatherapp
 import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.os.Build
 import android.os.Bundle
+import android.text.format.DateFormat
 import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import org.json.JSONObject
-import java.io.InputStream
 import java.io.InputStreamReader
-import java.net.HttpURLConnection
 import java.net.URL
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
 import java.util.*
 import javax.net.ssl.HttpsURLConnection
 
@@ -26,6 +32,7 @@ class WeatherActivity : AppCompatActivity() {
     private val lang = "fr"
     private val unit = "metric"
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_weather)
@@ -33,6 +40,7 @@ class WeatherActivity : AppCompatActivity() {
         fetchWeather().start()
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun fetchWeather(): Thread {
         return Thread {
             val url = URL(
@@ -50,6 +58,10 @@ class WeatherActivity : AppCompatActivity() {
                 val inputStreamReader = InputStreamReader(inputSystem, "UTF-8")
                 val response = JSONObject(inputStreamReader.readText())
 
+                val inputSystemForecast = connectionForecast.inputStream
+                val inputStreamReaderForecast = InputStreamReader(inputSystemForecast, "UTF-8")
+                val responseForecast = JSONObject(inputStreamReaderForecast.readText())
+
                 val urlIcon = URL(ICON_URL.get(0) + (response.getJSONArray("weather")[0] as JSONObject).getString("icon") + ICON_URL.get(1))
 
                 val connectionIcon = urlIcon.openConnection() as HttpsURLConnection
@@ -57,43 +69,59 @@ class WeatherActivity : AppCompatActivity() {
                     val inputStreamIcon = connectionIcon.inputStream
                     val bmp = BitmapFactory.decodeStream(inputStreamIcon)
 
-                    updateUITodayWeather(response, bmp)
+                    updateUITodayWeather(response, responseForecast, bmp)
+
+                    inputStreamIcon.close()
+
                 } else {
                     //TODO
                 }
 
-
                 inputStreamReader.close()
                 inputSystem.close()
+
+                inputStreamReaderForecast.close()
+                inputSystemForecast.close()
             } else {
                 Log.d("error : ", "error")
             }
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     @SuppressLint("SetTextI18n")
-    private fun updateUITodayWeather(response: JSONObject, bmp :Bitmap) {
+    private fun updateUITodayWeather(current: JSONObject, forecast: JSONObject, bmp :Bitmap) {
         runOnUiThread {
             kotlin.run {
                 val cityTv = findViewById<TextView>(R.id.tv_city_name)
                 val currentTempTv = findViewById<TextView>(R.id.tv_current_temp)
                 val weatherDescTv = findViewById<TextView>(R.id.tv_weather_desc)
                 val minMaxTempTv = findViewById<TextView>(R.id.tv_min_max_temp)
-                val feelsLikeTv = findViewById<TextView>(R.id.feels_like_tv)
+                val feelsLikeTv = findViewById<TextView>(R.id.tv_feels_like)
+                val timeTv = findViewById<TextView>(R.id.tv_time)
 
                 val weatherIcon = findViewById<ImageView>(R.id.weather_icon)
                 weatherIcon.setImageBitmap(bmp)
 
-                val weather = response.getJSONArray("weather")[0] as JSONObject
-                val main = response.getJSONObject("main")
+                val weather = current.getJSONArray("weather")[0] as JSONObject
+                val main = current.getJSONObject("main")
 
-                cityTv.text = response.getString("name")
+                cityTv.text = current.getString("name")
                 currentTempTv.text = main.getString("temp").dropLast(3) + "°C"
                 weatherDescTv.text = weather.getString("description")
                     .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
                 minMaxTempTv.text =
                     "Max. " + main.getString("temp_max").dropLast(3) + "°C Min. " + main.getString("temp_min").dropLast(3) + "°C"
                 feelsLikeTv.text = getString(R.string.feels_like) + " " + main.getString("feels_like").dropLast(3) + "°C"
+
+                Log.e( "updateUITodayWeather: ", current.getInt("dt").toString())
+                val simpleDateFormat = SimpleDateFormat("dd MMM HH:mm")
+
+
+
+                // val date = Date(current.getLong("dt") * 1000, simpleDateFormat)
+                timeTv.text = simpleDateFormat.format(current.getInt("dt") * 1000L + 3600000)
+
             }
         }
 
